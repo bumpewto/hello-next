36fe078dd7b846b022103c2ed2a6bbe8328bfaee
bumpewto <39131038+bumpewto@users.noreply.github.com>
2019-03-31 15:32:07 +0200

    :lipstick: STYLE:
    Fix Hero display

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ import EarlyBirdSubscribe from "../components/EarlyBirdSubscribe";
 
 // remplacer grad par img
 const Hero = styled.div`
-  display; flex;
-  
+  display: flex;
+  flex-direction: column;
   background: cornflowerblue;
   height: 100vh;
   padding: 6rem 2rem 2rem;
