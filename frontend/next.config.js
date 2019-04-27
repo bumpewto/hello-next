4dd5d07a69c5f3d6233a7a30b3274dd9c7708062
bumpewto <t.boumpoutou@gmail.com>
2019-04-28 01:22:17 +0200

    :bug: fix service worker not served

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,76 +1,71 @@
-module.exports = {
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+const withPlugins = require("next-compose-plugins");
+const withOffline = require("next-offline");
+
+const nextConfig = {
     target: "serverless",
 };
 
-// Serving Service Worker
-
-const withOffline = moduleExists("next-offline") ? require("next-offline") : {};
-
-const nextConfig = {
-    workboxOpts: {
-        swDest: "static/service-worker.js",
-        clientsClaim: true,
-        skipWaiting: true,
-        // Set Prefix for serving serviceworker.js from /static
-        globPatterns: ["frontend/static/**/*"],
-        globDirectory: ".",
-        // modifyUrlPrefix: {
-        //     frontend: assetPrefix,
-        // },
-        runtimeCaching: [
+module.exports = withPlugins(
+    [
+        // add a plugin with specific configuration
+        [
+            withBundleAnalyzer,
             {
-                urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
-                handler: "cacheFirst",
-                options: {
-                    cacheName: "image-cache",
-                    cacheableResponse: {
-                        statuses: [0, 200],
+                analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+                analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+                bundleAnalyzerConfig: {
+                    server: {
+                        analyzerMode: "static",
+                        reportFilename: "../bundles/server.html",
                     },
-                },
-            },
-            {
-                urlPattern: /^https?.*/,
-                handler: "networkFirst",
-                options: {
-                    cacheName: "https-calls",
-                    networkTimeoutSeconds: 15,
-                    expiration: {
-                        maxEntries: 150,
-                        maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-                    },
-                    cacheableResponse: {
-                        statuses: [0, 200],
+                    browser: {
+                        analyzerMode: "static",
+                        reportFilename: "../bundles/client.html",
                     },
                 },
             },
         ],
-    },
-};
-
-module.exports = moduleExists("next-offline") ? withOffline(nextConfig) : nextConfig;
-
-function moduleExists(name) {
-    try {
-        return require.resolve(name);
-    } catch (error) {
-        return false;
-    }
-}
-
-// Webpack Bundle Analyzer
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
-
-module.exports = withBundleAnalyzer({
-    analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-        server: {
-            analyzerMode: "static",
-            reportFilename: "../bundles/server.html",
-        },
-        browser: {
-            analyzerMode: "static",
-            reportFilename: "../bundles/client.html",
-        },
-    },
-});
+        [
+            withOffline,
+            {
+                generateInDevMode: true,
+                workboxOpts: {
+                    swDest: "static/sw.js",
+                    clientsClaim: true,
+                    skipWaiting: true,
+                    globPatterns: ["static/**/*"],
+                    globDirectory: ".",
+                    runtimeCaching: [
+                        {
+                            urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
+                            handler: "cacheFirst",
+                            options: {
+                                cacheName: "image-cache",
+                                cacheableResponse: {
+                                    statuses: [0, 200],
+                                },
+                            },
+                        },
+                        {
+                            urlPattern: /^https?.*/,
+                            handler: "networkFirst",
+                            options: {
+                                cacheName: "https-calls",
+                                networkTimeoutSeconds: 15,
+                                expiration: {
+                                    maxEntries: 150,
+                                    maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+                                },
+                                cacheableResponse: {
+                                    statuses: [0, 200],
+                                },
+                            },
+                        },
+                    ],
+                },
+            },
+        ],
+    ],
+    nextConfig
+);
