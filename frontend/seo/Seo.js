e04723b7af5b43363f9bf39abe9ac03eddba8658
bumpewto <t.boumpoutou@gmail.com>
2019-04-30 01:09:28 +0200

    remove seo component, must inject title and meta directly

--- frontend/seo/Seo.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-
-const SEO = ({ title, description }) => (
-  <Head>
-    <title>{title || "Atmo"}</title>
-    <meta description={description || ""} />
-  </Head>
-);
-export default SEO;
